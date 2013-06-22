78aa7b711469dee6ff6a8865575b80c2453bcf5d
Nathanael Mann <nathanael.mann@cit-design.co.uk>
2013-06-22 12:19:30 +0100

    Missing Files

---
+++ Lucinq.Sitecore/Querying/SitecoreItemResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Lucinq.SitecoreIntegration.Querying.Interfaces;
+using Sitecore.Data.Items;
+
+namespace Lucinq.SitecoreIntegration.Querying
+{
+	public class SitecoreItemResult : ISitecoreItemResult
+	{
+		#region [ Constructors ]
+
+		public SitecoreItemResult(List<Item> items)
+		{
+			Items = items;
+		}
+
+		#endregion
+
+		#region [ Properties ]
+
+		public List<Item> Items { get; private set; }
+
+		public long ElapsedTimeMs { get; set; }
+
+		public int TotalHits { get; set; }
+
+		#endregion
+	}
+}
