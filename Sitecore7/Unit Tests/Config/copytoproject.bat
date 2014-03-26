5cf5c77e6ec27e26843a8ac73c1d409da7ff169e
Simon <simon@simonproctor.com>
2014-03-26 06:41:49 +0000

    Fixed issue with config copying

--- Sitecore7/Unit Tests/Config/copytoproject.bat
+++ Sitecore7/Unit Tests/Config/copytoproject.bat
@@ -1,11 +1,11 @@
 echo off
 echo "executing: copysiteoutput.bat %1"
 
-echo copying app_config  to %1app_config
-robocopy %1..\config\app_config %1App_config /e /xf *.cs *.bak connectionstrings.* /nfl /ndl /njh /njs /ns /nc /np > nul
+echo copying %1..\..\..\config\app_config  to %1app_config
+robocopy %1..\..\..\config\app_config %1App_config /e /xf *.cs *.bak connectionstrings.* /nfl /ndl /njh /njs /ns /nc /np
 
 echo copying app.config
-robocopy %1..\config %1 App.Config /e /xf *.cs *.bak /nfl /ndl /njh /njs /ns /nc /np > nul
+robocopy %1..\..\..\config %1..\..\ App.Config /e /xf *.cs *.bak /nfl /ndl /njh /njs /ns /nc /np > nul
 
 if errorlevel 7 echo OKCOPY + MISMATCHES + XTRA & goto end1
 if errorlevel 5 echo OKCOPY + MISMATCHES & goto end1
